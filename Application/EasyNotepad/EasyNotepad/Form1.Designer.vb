a9f1a6afe9cd1224098b891cecf1491eb68d5a89
zoLovro <lovrohvalakopar@gmail.com>
2016-01-29 15:33:14 +0100

    Application

---
+++ Application/EasyNotepad/EasyNotepad/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.QuickSaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.QuickOpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.QuickDeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.CreditsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.Panel1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(637, 491)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.RichTextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 31)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(637, 491)
+        Me.Panel1.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.CreditsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(661, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.QuickSaveToolStripMenuItem, Me.QuickOpenToolStripMenuItem, Me.QuickDeleteToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'QuickSaveToolStripMenuItem
+        '
+        Me.QuickSaveToolStripMenuItem.Name = "QuickSaveToolStripMenuItem"
+        Me.QuickSaveToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.QuickSaveToolStripMenuItem.Text = "Quick Save"
+        '
+        'QuickOpenToolStripMenuItem
+        '
+        Me.QuickOpenToolStripMenuItem.Name = "QuickOpenToolStripMenuItem"
+        Me.QuickOpenToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.QuickOpenToolStripMenuItem.Text = "Quick Open"
+        '
+        'QuickDeleteToolStripMenuItem
+        '
+        Me.QuickDeleteToolStripMenuItem.Name = "QuickDeleteToolStripMenuItem"
+        Me.QuickDeleteToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.QuickDeleteToolStripMenuItem.Text = "Quick Delete"
+        '
+        'CreditsToolStripMenuItem
+        '
+        Me.CreditsToolStripMenuItem.Name = "CreditsToolStripMenuItem"
+        Me.CreditsToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.CreditsToolStripMenuItem.Text = "Credits"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(661, 534)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Easy Notepad"
+        Me.Panel1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents QuickSaveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CreditsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents QuickOpenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents QuickDeleteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Application/EasyNotepad/EasyNotepad/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub QuickSaveToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles QuickSaveToolStripMenuItem.Click
+
+        'QuickSave
+        Dim text As String
+        text = RichTextBox1.Text
+        My.Settings.quickSaveLine = text
+
+
+    End Sub
+
+    Private Sub QuToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles QuickOpenToolStripMenuItem.Click
+
+        'Quick open
+        RichTextBox1.Text = My.Settings.quickSaveLine
+
+    End Sub
+
+    Private Sub QuickDeleteToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles QuickDeleteToolStripMenuItem.Click
+
+        My.Settings.quickSaveLine = ""
+
+    End Sub
+
+    Private Sub CreditsToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CreditsToolStripMenuItem.Click
+        MsgBox("Made by Zetko and Lovro")
+    End Sub
+End Class
